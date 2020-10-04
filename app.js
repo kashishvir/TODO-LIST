31d40d40c3e47e7a3ab740d0a42821d8f1b853d1
kashish singh <kashishsingh@kashishs-MacBook-Air.local>
2020-10-04 22:21:46 +0400

    add gitignore,procfile and update ports

--- app.js
+++ app.js
@@ -139,7 +139,11 @@ app.get("/:customListName",function(req,res)
 app.get("/about", function(req, res){
   res.render("about");
 });
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-app.listen(process.env.PORT||3000, function() {
-  console.log("Server started on port 3000");
+app.listen(port, function() {
+  console.log("Server started successfully");
 });
